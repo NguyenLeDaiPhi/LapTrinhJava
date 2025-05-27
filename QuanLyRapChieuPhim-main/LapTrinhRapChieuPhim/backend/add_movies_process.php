19eeff78cc18d114879fade7a57830e98a011233
NguyenLeDaiPhi <nguyenledaiphi0252005@gmail.com>
2025-05-27 21:51:19 +0700

    Delete QuanLyRapChieuPhim-main directory

--- QuanLyRapChieuPhim-main/LapTrinhRapChieuPhim/backend/add_movies_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'movies.php';
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $movies = new Movies();
-    $movies->addMovie($_POST["title"], $_POST["genre"], $_POST["duration"], $_POST["release_date"], $_POST["showtimes"], $_POST["price"], $_FILES['poster'], $_POST["description"], $_POST["trailer_link"], date("Y-m-d H:i:s"));
-}
-?>
