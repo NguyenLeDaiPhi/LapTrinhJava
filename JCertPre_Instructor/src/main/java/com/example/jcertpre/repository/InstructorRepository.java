3c9df8ffef3520aa6ca9638a763631d74ce2e911
NguyenLeDaiPhi <nguyenledaiphi0252005@gmail.com>
2025-06-13 15:04:42 +0700

    Delete JCertPre_Instructor directory

--- JCertPre_Instructor/src/main/java/com/example/jcertpre/repository/InstructorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jcertpre.repository;
-
-import com.example.jcertpre.model.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-    Instructor findByEmail(String email);
-}
