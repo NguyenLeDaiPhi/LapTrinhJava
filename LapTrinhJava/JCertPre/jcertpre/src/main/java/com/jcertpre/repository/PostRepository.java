02df562197d8a181321b199926401ae6acdea73d
Tcongly <truongcongly2ka5@gmail.com>
2025-06-23 15:09:29 +0700

    add

--- LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/repository/PostRepository.java
+++ LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.jcertpre.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jcertpre.model.Post;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
     
 }
