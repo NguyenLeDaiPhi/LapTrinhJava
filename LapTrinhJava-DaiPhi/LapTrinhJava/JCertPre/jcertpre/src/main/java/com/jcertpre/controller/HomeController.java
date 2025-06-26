3a13cc1f1fbda82005042ef34da9d637d7f118d8
Nguyen Le Dai Phi <nguyenledaiphi0252005@gmail.com>
2025-06-26 16:49:59 +0700

    Push take exam simulation for instructor

---
+++ LapTrinhJava-DaiPhi/LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.jcertpre.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index"; // Loads index.html from templates/
+    }
+
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about"; // Loads about.html from templates/
+    }
+
+    @GetMapping("/teacher") 
+    public String showTeacherPage() {
+        return "teacher"; // Loads teacher.html from templates/
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage() {
+        return "contact"; // Loads contact.html from templates/
+    }
+
+    @GetMapping("/blog")
+    public String showBlogPage() {
+        return "blog"; // Loads blog.html from templates/
+    }
+
+    @GetMapping("/single")
+    public String showSinglePage() {
+        return "single"; // Loads single.html from templates/
+    }
+    
+
+}
---
+++ LapTrinhJava-DaiPhi/LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/repository/ExamSimulationRepository.java
@@ -0,0 +1,9 @@
+package com.jcertpre.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jcertpre.model.ExamSimulation;
+
+public interface ExamSimulationRepository extends JpaRepository<ExamSimulation, Long>{
+    
+}
