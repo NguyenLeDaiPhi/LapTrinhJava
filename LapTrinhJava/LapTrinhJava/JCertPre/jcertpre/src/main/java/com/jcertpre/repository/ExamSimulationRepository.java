1f7fe9c843e95fb8901a1345216d1de247be732f
Nguyen Le Dai Phi <nguyenledaiphi0252005@gmail.com>
2025-06-26 15:15:18 +0700

    Remove LapTrinhJava folder

--- LapTrinhJava/LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/repository/ExamSimulationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jcertpre.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jcertpre.model.ExamSimulation;
-
-public interface ExamSimulationRepository extends JpaRepository<ExamSimulation, Long>{
-    
-}
--- LapTrinhJava/LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jcertpre.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jcertpre.model.Notification;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long>{
-    
-}
