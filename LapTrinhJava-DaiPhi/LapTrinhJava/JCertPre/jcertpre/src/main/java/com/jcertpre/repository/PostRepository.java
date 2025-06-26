d2e95d2e4055c80d63453c13da6bc1c93b479e96
Nguyen Le Dai Phi <nguyenledaiphi0252005@gmail.com>
2025-06-26 16:29:03 +0700

    Your commit message

--- LapTrinhJava-DaiPhi/LapTrinhJava/JCertPre/jcertpre/src/main/java/com/jcertpre/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jcertpre.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jcertpre.model.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long>{
-    
-}
